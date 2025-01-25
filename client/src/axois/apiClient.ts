7f4c44fd071448da7d0f3ed9acf3be0e4957f02c
vishnu KP <vishnuto16@gmail.com>
2025-01-25 07:05:37 +0530

    commit

--- client/src/axois/apiClient.ts
+++ client/src/axois/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cafe-project-1-h8hj.onrender.com",
+  baseURL: "https",
   headers: {
     "Content-Type": "application/json",
   },
