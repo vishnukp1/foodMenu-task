5c7a4a1ddbb467d38e4fcef2f53542057a571498
vishnu KP <vishnuto16@gmail.com>
2025-01-25 07:06:29 +0530

    update the baseurl

--- client/src/axois/apiClient.ts
+++ client/src/axois/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https",
+  baseURL: "https://foodmenu-task.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
